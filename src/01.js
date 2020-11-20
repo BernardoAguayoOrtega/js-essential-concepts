aed9a7a71e6a32d481d0f7bdc9464df24666b1de
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-20 09:16:30 -0600

    Create app class and entry point inside index.html

--- src/01.js
+++ src/01.js
@@ -1,2 +1,15 @@
 // Modules -- allow us to share code between files
 
+class App {
+  constructor() {
+    this.render()
+  }
+
+  render() {
+    document.getElementById('root').innerHTML = `
+      <h1> Date: <3 </h1>
+    ` 
+  }
+}
+
+app = new App()
