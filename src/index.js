46bfece376b96acdc457bec99b97e7c59ae562e4
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-23 09:20:05 -0600

    Update files

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { getDate, year } from '../utils/date.js';
 class App {
 	constructor() {
 		this.render();
+		this.render2();
 	}
 
 	render() {
 		document.getElementById('root').innerHTML = `
       <h1> Date: ${year}<3 </h1>
-    `;
+		`;
 	}
 }
 
