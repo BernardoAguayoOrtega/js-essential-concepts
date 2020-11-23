9f99f50df04e11ebb78e693e3a806905dca66073
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-23 09:28:54 -0600

    Create this file

---
+++ src/this.js
@@ -0,0 +1,13 @@
+/*
+
+this: reference to an object
+
+4 Rules to 'this'. How is it called?
+
+1) in the global context
+2) as a method on an object
+3) as a constructor function or class constructor
+4) as a DOM event handler
+
+*/
+
