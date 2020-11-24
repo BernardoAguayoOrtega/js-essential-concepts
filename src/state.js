f908cd0cf7789af857f8643049da4355f8ab67e3
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-24 13:07:32 -0600

    Apply stat in our app

--- src/state.js
+++ src/state.js
@@ -6,38 +6,38 @@
 // state -> status
 
 class App {
-  constructor() {
-    this.state = {
-       isAuth: false,
-       error: ''  
-    };  
-      
-    this.checkAuth();
-    this.render();
-    // this.$userMessage = document.getElementById("user-message");
-  }
+	constructor() {
+		this.state = {
+			isAuth: false,
+			error: '',
+		};
 
-  checkAuth() {
-    const user = false;
-    if (user) {
-      this.state = { ...this.state, isAuth: true };
-    //   this.$userMessage.textContent = "Welcome back!";
-    } else {
-      this.state = { ...this.state, error: "You must sign in!" };
-    //   this.$userMessage.textContent = "You must sign in!";
-    //   this.$userMessage.style.color = "red";
-    }
-  }
+		this.checkAuth();
+		this.render();
+		// this.$userMessage = document.getElementById("user-message");
+	}
 
-  render() {
-    const { isAuth, error } = this.state; 
-      
-    document.getElementById("root").innerHTML = `
+	checkAuth() {
+		const user = false;
+		if (user) {
+			this.state = { ...this.state, isAuth: true };
+			//   this.$userMessage.textContent = "Welcome back!";
+		} else {
+			this.state = { ...this.state, error: 'You must sign in!' };
+			//   this.$userMessage.textContent = "You must sign in!";
+			//   this.$userMessage.style.color = "red";
+		}
+	}
+
+	render() {
+		const { isAuth, error } = this.state;
+
+		document.getElementById('root').innerHTML = `
       <div style="color: ${error && 'red'}">
         ${isAuth ? 'Welcome back!' : error}
       </div>
     `;
-  }
+	}
 }
 
 new App();
