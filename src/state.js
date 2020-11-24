87af2a894ee6b9d859d88dc820f30eaa2c3844ca
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-24 12:34:31 -0600

    init the file

--- src/state.js
+++ src/state.js
@@ -1,1 +1,31 @@
 // state - data to be managed in app
+// state -> status
+// state is important because say us the status of our application
+
+class App {
+  constructor() {
+    this.render();
+    this.$userMessage = document.getElementById("user-message");
+    this.checkAuth();
+  }
+
+  checkAuth() {
+    const user = true;
+    if (user) {
+      this.$userMessage.textContent = "Welcome back!";
+    } else {
+      this.$userMessage.textContent = "You must sign in!";
+      this.$userMessage.style.color = "red";
+    }
+  }
+
+  render() {
+    document.getElementById("root").innerHTML = `
+      <div>
+        <span id="user-message"></span>
+      </div>
+    `;
+  }
+}
+
+new App();
