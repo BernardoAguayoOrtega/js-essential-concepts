ee4c507a5d4654ded8718b1a58244fe1fc963914
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-25 08:39:21 -0600

    Implement counter reducer with switch statment

--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,20 @@
 // reducers - helps us manage app state
 // const reducer = (state, action) => newState;
 
-function counterReducer(state = { count: 0 }, action) {
+function counterReducer_(state = { count: 0 }, action) {
 	return { ...state, count: state.count + 1 };
 }
 
-console.log(counterReducer())
-console.log(counterReducer({count: 1}))
-console.log(counterReducer({count: 2}))
-console.log(counterReducer({count: 3}))
+console.log(counterReducer_())
+console.log(counterReducer_({count: 1}))
+console.log(counterReducer_({count: 2}))
+console.log(counterReducer_({count: 3}))
+
+function counterReducer(state = {count: 0}, action){
+  const { type } = action
+  switch(type){
+    case 'INCREMENT': return { ...state, count: state.count + 1 };
+    case 'DECREMENT': return { ...state, count: state.count - 1 };
+    default: return state;
+  }
+}
