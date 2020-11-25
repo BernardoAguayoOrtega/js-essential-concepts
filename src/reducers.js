1d558819efd9f7b71881d083153070bbfdb9630f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-25 08:43:54 -0600

    Test switch statmenet inside my reducer

--- src/reducers.js
+++ src/reducers.js
@@ -10,7 +10,7 @@ console.log(counterReducer_({count: 1}))
 console.log(counterReducer_({count: 2}))
 console.log(counterReducer_({count: 3}))
 
-function counterReducer(state = {count: 0}, action){
+function counterReducer(state = {count: 0}, action = { action: '' }){
   const { type } = action
   switch(type){
     case 'INCREMENT': return { ...state, count: state.count + 1 };
@@ -18,3 +18,7 @@ function counterReducer(state = {count: 0}, action){
     default: return state;
   }
 }
+console.log(counterReducer())
+console.log(counterReducer({count: 1}, {type: 'INCREMENT'}))
+console.log(counterReducer({count: 2}, {type: 'INCREMENT'}))
+console.log(counterReducer({count: 3}, {type: 'INCREMENT'}))
