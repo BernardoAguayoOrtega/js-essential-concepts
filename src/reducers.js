48ebf733111c2367df82bf12c06e5b2dbd78233a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-25 08:30:50 -0600

    First reduce => counter reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,1 +1,11 @@
 // reducers - helps us manage app state
+// const reducer = (state, action) => newState;
+
+function counterReducer(state = { count: 0 }, action) {
+	return { ...state, count: state.count + 1 };
+}
+
+console.log(counterReducer())
+console.log(counterReducer({count: 1}))
+console.log(counterReducer({count: 2}))
+console.log(counterReducer({count: 3}))
